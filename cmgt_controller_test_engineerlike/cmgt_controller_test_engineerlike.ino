28a999de0ebd7375345fd80f2009308792432b00
Merlijn Wegen <merlijn.wegen@gmail.com>
2023-02-23 16:01:31 +0100

    added p2

--- cmgt_controller_test_engineerlike/cmgt_controller_test_engineerlike.ino
+++ cmgt_controller_test_engineerlike/cmgt_controller_test_engineerlike.ino
@@ -1,9 +1,9 @@
 #include <Keyboard.h>
 
 // use this option for OSX:
-char ctrlKey = KEY_LEFT_GUI;
+//char ctrlKey = KEY_LEFT_GUI;
 // use this option for Windows and Linux:
-//  char ctrlKey = KEY_LEFT_CTRL;
+  char ctrlKey = KEY_LEFT_CTRL;
 
 void setup() {
   delay(3000);
@@ -11,13 +11,18 @@ void setup() {
   // make pin 2 an input and turn on the
   // pullup resistor so it goes high unless
   // connected to ground:
-  pinMode(3, INPUT_PULLUP); //JOYSTICK RIGHT
-  pinMode(4, INPUT_PULLUP); //JOYSTICK LEFT
-  pinMode(5, INPUT_PULLUP); //JOYSTICK DOWN
-  pinMode(6, INPUT_PULLUP); //JOYSTICK UP
+  pinMode(12, INPUT_PULLUP); //JOYSTICK RIGHT
+  pinMode(11, INPUT_PULLUP); //JOYSTICK LEFT
+  pinMode(9, INPUT_PULLUP); //JOYSTICK DOWN
+  pinMode(10, INPUT_PULLUP); //JOYSTICK UP
+  
+  pinMode(6, INPUT_PULLUP); //JOYSTICK 2 RIGHT
+  pinMode(5, INPUT_PULLUP); //JOYSTICK 2 LEFT
+  pinMode(3, INPUT_PULLUP); //JOYSTICK 2 DOWN
+  pinMode(4, INPUT_PULLUP); //JOYSTICK 2 UP
 
-  pinMode(14, INPUT_PULLUP); //PLAYER 1 ATTACK
-  pinMode(15, INPUT_PULLUP); //PLAYER 1 DEFEND
+  pinMode(18, INPUT_PULLUP); //PLAYER 1 ATTACK
+  pinMode(19, INPUT_PULLUP); //PLAYER 1 DEFEND
   pinMode(20, INPUT_PULLUP); //PLAYER 2 ATTACK
   pinMode(21, INPUT_PULLUP); //PLAYER 2 DEFEND
 
@@ -26,42 +31,117 @@ void setup() {
 }
 
 void loop() {
-  //JOYSTICK RIGHT
+  //JOYSTICK RIGHT player 1
   if (!digitalRead(3) == HIGH) {
   Keyboard.press('d');
   Serial.println("d pressed");
   }
+  else{
+    Keyboard.release('d');
+  }
 
-  //JOYSTICK LEFT
+  //JOYSTICK LEFT player 1
   if (!digitalRead(4) == HIGH) {
   Keyboard.press('a');
   Serial.println("a pressed");
   }
+  else{
+    Keyboard.release('a');
+  }
 
-  //JOYSTICK DOWN
+  //JOYSTICK DOWN player 1
   if (!digitalRead(5) == HIGH) {
   Keyboard.press('s');
   Serial.println("s pressed");
   }
+  else{
+    Keyboard.release('s');
+  }
 
-  //JOYSTICK UP
+  //JOYSTICK UP player 1
   if (!digitalRead(6) == HIGH) {
   Keyboard.press('w');
   Serial.println("w pressed");
   }
+  else{
+    Keyboard.release('w');
+  }
 
+  //attack player 1
   if (!digitalRead(14) == HIGH) {
   Keyboard.press('e');
   Serial.println("e pressed - attack 1");
   }
-  
+  else{
+    Keyboard.release('e');
+  }
+
+  //defend player 1
   if (!digitalRead(15) == HIGH) {
   Keyboard.press('r');
   Serial.println("r pressed - defend 1");
   }
-  delay(100);
+  else{
+    Keyboard.release('r');
+  }
 
-  Keyboard.releaseAll();
+
+
+  
+
+  //JOYSTICK RIGHT player 2
+  if (!digitalRead(6) == HIGH) {
+  Keyboard.press('l');
+  Serial.println("l pressed");
+  }
+  else{
+    Keyboard.release('l');
+  }
+
+  //JOYSTICK LEFT player 2
+  if (!digitalRead(5) == HIGH) {
+  Keyboard.press('j');
+  Serial.println("j pressed");
+  }
+  else{
+    Keyboard.release('j');
+  }
+
+  //JOYSTICK DOWN player 2
+  if (!digitalRead(3) == HIGH) {
+  Keyboard.press('k');
+  Serial.println("k pressed");
+  }
+  else{
+    Keyboard.release('k');
+  }
+
+  //JOYSTICK UP player 2
+  if (!digitalRead(4) == HIGH) {
+  Keyboard.press('i');
+  Serial.println("i pressed");
+  }
+  else{
+    Keyboard.release('i');
+  }
+
+  //attack player 2
+  if (!digitalRead(18) == HIGH) {
+  Keyboard.press('o');
+  Serial.println("o pressed - attack 2");
+  }
+  else{
+    Keyboard.release('o');
+  }
+
+  //defend player 1
+  if (!digitalRead(19) == HIGH) {
+  Keyboard.press('p');
+  Serial.println("p pressed - defend 2");
+  }
+  else{
+    Keyboard.release('p');
+  }
   // wait for new window to open:
 
 }
